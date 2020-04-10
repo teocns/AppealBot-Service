7857bca376c6ea527451474a77d033a5af92d5a2
teocns <teocns@gmail.com>
2020-04-10 21:11:31 +0200

    asd

--- subscriptions_service.py
+++ subscriptions_service.py
@@ -2,11 +2,11 @@ from api import req
 from time import sleep
 from helpers import prttime
 while True:
-    sleep(1)
-    
-    data = req('subscription_service');
-    if data:
-        print(prttime() + data['message'])
-    else:
-        print(prttime() + " No accounts found")
-    
+	sleep(1)
+
+	data = req('subscription_service')
+	if data:
+		print(f'[{prttime()}] ' + data['message'])
+	else:
+		print(f'[{prttime()}] No accounts found')
+		sleep(30)
