d85652060884af366cdf4fb1bbd0f4ba11b1b85f
teocns <teocns@gmail.com>
2020-04-10 18:24:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 import requests
-import api
+from api import req
 
 
- current_email_data = {
-            'appeal_process_id':  data['id'],
-            'time_fetched': int(time.time()),
-            'time_received':email['timestamp'],
-            'body': str(email['body']),
-            'status': email['status'],
-            'message_id': email['message_id'],
-            'from': str(email['from']),
-            'code': email['code'],
-            'subject': str(email['subject']),
-            'email_id':data['email_id']
-        }
-print(f"[{prttime()}] Requesting to store email ({email['status']}) for {data['ig_account_username']}")
+email = b"Salve,\n\nTi ringraziamo per averci contattato. Prima di poterti fornire assistenza, dobbiamo verificare che tu sia il titolare dell'account. \n\nTi preghiamo di rispondere a questa e-mail allegando una tua foto in cui reggi il codice scritto a mano riportato di seguito.\n\n80867\n\nAssicurati che la foto che invii:\n\n- mostri il codice sopra riportato scritto su un foglio di carta e seguito dal tuo nome completo e dal tuo nome utente;\n- mostri le tue mani che reggono il foglio e il tuo viso per intero;\n- sia ben illuminata, non sia troppo piccola, scura o sfocata;\n- sia allegata alla tua risposta come file JPEG.\n\nTieni presente che anche se questo account non comprende foto tue o viene usato per rappresentare qualcun altro o qualcos'altro, non potremo fornirti assistenza finch\xc3\xa9 non riceviamo una foto che soddisfi questi requisiti.\n\nGrazie,\nIl team di Instagram\n\n"
+
+current_email_data = {
+    'appeal_process_id':  123,
+    'time_fetched': int(time.time()),
+    'time_received':int(time.time()) + 132994949,
+    'body': str(email),
+    'status': 'CODE_RECEIVED',
+    'message_id': email['message_id'],
+    'from': str('Facebook <instagram++aazqggbkbltwbj@support.facebook.com>'),
+    'code': '80867',
+    'subject': str('My Instagram Account Was Deactivated'),
+    'email_id':3088
+}
+
 result = req('register_email_received',data = current_email_data)
+
+print (result['message'])
