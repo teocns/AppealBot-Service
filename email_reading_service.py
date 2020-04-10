51a8f6788f91023c6802798f9cbd9866a5436ea8
teocns <teocns@gmail.com>
2020-04-10 12:20:20 +0200

    Update email_reading_service.py

--- email_reading_service.py
+++ email_reading_service.py
@@ -42,7 +42,7 @@ while True:
             'subject': str(email['subject']),
             'email_id':data['email_id']
         }
-        print(f"[{prttime()}] Requesting to store email for {data['ig_account_username']}")
+        print(f"[{prttime()}] Requesting to store email ({email['status']}) for {data['ig_account_username']}")
         result = req('register_email_received',data = current_email_data)
         if result['delete_email']:
             print(f"Deleting email {email['index']} as per backend request")
