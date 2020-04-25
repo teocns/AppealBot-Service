dec1226b44eaff223ecc4a05fda11725cc607634
teocns <teocns@gmail.com>
2020-04-25 07:03:41 +0200

    Create pop3_test.py

---
+++ api/pop3_test.py
@@ -0,0 +1,49 @@
+import pprint
+import email
+import poplib
+import re
+
+import time
+import datetime
+import calendar
+
+from email.header import decode_header
+
+
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('email')
+parser.add_argument('password')
+parser.add_argument('host')
+args = parser.parse_args()
+
+
+def pop3_test(email_ru, password):
+	try:
+		SRV = "pop.mail.ru"
+		PORT = 995
+
+		USER = email_ru
+		PASSWORD = password
+
+		mail_box = poplib.POP3_SSL(SRV, PORT)
+		
+		mail_box.user(USER)
+		mail_box.pass_(PASSWORD)
+		num_messages = None
+
+		num_messages = None
+
+		num_messages = len(mail_box.list()[1])
+		mail_box.quit()
+		return True
+	except:
+		return False
+
+print ( pop3_test(args.email,args.password) )
