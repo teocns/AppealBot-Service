8cc2cf561a6e6c33c9459c0b59cd1ab7ef98406b
teocns <teocns@gmail.com>
2020-05-16 00:03:57 +0200

    Update post_detection.py

--- papersheet-detection/post_detection.py
+++ papersheet-detection/post_detection.py
@@ -59,6 +59,21 @@ def getAccurateBox(imagePath,detection):
     detection_x1 =   int( detection['center_x'] ) - int(detection_width/2)
     detection_y1 =   int( detection['center_y'] ) - int(detection_height/2)
     
+    old_box = {
+        "p1":[detection_x1,detection_y1],
+        "p2":[detection_x1+width,detection_y1],
+        "p3":[detection_x1+width,detection_y1+height],
+        "p4":[detection_x1,detection_y1+height]
+    }
     cropped =  img[detection_y1:detection_y1+detection_height,detection_x1:detection_x1+detection_width]
     
-    return generateCoordinates(cropped)
+    box =  generateCoordinates(cropped)
+    old_box["p1"][0] +=  box[0][2][0]
+    old_box["p1"][1] +=  box[0][2][1]
+    old_box["p2"][0] -=  box[0][3][0]
+    old_box["p2"][1] +=  box[0][3][1]
+    old_box["p3"][0] -=  box[0][0][0]
+    old_box["p3"][1] -=  box[0][0][1]
+    old_box["p4"][0] +=  box[0][1][0]
+    old_box["p4"][1] -=  box[0][1][1]
+    return old_box
