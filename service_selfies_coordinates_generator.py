f0deaf4c42802ddbbad97f677340ba7a74098a3a
teocns <teocns@gmail.com>
2020-05-15 17:05:40 +0200

    Update service_selfies_coordinates_generator.py

--- service_selfies_coordinates_generator.py
+++ service_selfies_coordinates_generator.py
@@ -13,27 +13,23 @@ SLASH = str( '\\' if os.name == 'nt' else '/' )
 
 sys.path.insert(0, '/var/AI/darknet/')
 import darknet
+darknet.performDetect(initOnly=True)
 
-
-
-exit(
-    darknet.performDetect('/var/appealbot/selfies/9-2000-1821-2973-1493-3293-2215-2219-2580-70.jpg')
-)
 while 1:
     
-    data = req('get_service_selfies_coordinates_generator')
-    if data:    
+    selfie = req('get_service_selfies_coordinates_generator')
+    if selfie:    
         with TemporaryDirectory() as tmpdir:
-            for selfie in data:
-                # Download selfie
-                download_url = "https://cdn.appealbot.net/"+selfie['filename']
-                tmpdirStr = str(tmpdir)
-                save_location = tmpdirStr+SLASH+selfie['filename']
-                urllib.request.urlretrieve(
-                    download_url,
-                    save_location
-                )
-                # Load detector (kewl)
+            # Download selfie
+            download_url = "https://cdn.appealbot.net/"+selfie['filename']
+            tmpdirStr = str(tmpdir)
+            save_location = tmpdirStr+SLASH+selfie['filename']
+            urllib.request.urlretrieve(
+                download_url,
+                save_location
+            )
+            print(darknet.performDetect('/var/appealbot/selfies/9-2000-1821-2973-1493-3293-2215-2219-2580-70.jpg'))
+            # Load detector (kewl)
                 
 
     
