1f0ca84082b6c73bd9b136e65f77db776db30102
teocns <teocns@gmail.com>
2020-05-16 20:30:43 +0200

    aaa

--- service_selfies_coordinates_generator.py
+++ service_selfies_coordinates_generator.py
@@ -20,57 +20,54 @@ with TemporaryDirectory() as tmpdir:
     while 1:
         selfie = req('get_service_selfies_coordinates_generator')
         if selfie:    
-            try:
-                # Download selfie
-                download_url = "https://cdn.appealbot.net/"+selfie['filename']
-                tmpdirStr = str(tmpdir)
-                save_location = tmpdirStr+SLASH+selfie['filename']
-                urllib.request.urlretrieve(
-                    download_url,
-                    save_location
-                )
-                print ('[DETECTOR] Performing for '+selfie['filename'])
-                detections = darknet.performDetect(save_location)
-                #detections = [('papersheet', 0.9260228872299194, (586.1337890625, 1471.395751953125, 992.4650268554688, 593.9584350585938))]
-                if len(detections) < 1:
-                    print ("Not found")
-                    req('set_service_selfies_coordinates_generator',{
-                        'selfie_id': selfie['id'],
-                        'coordinates': None
-                    });
-                else:
-                    # Get detection with highest confidence ( bruh )
-                    
-                    detection = sorted(detections,key= lambda x: x[1], reverse = True)[0]
-                    
-                    confidence = str(int(detection[1]*100)) + "%"
-                    
-                    c = getAccurateBox(save_location,{
-                        "center_x":detection[2][0],
-                        "center_y":detection[2][1],
-                        "width":detection[2][2],
-                        "height":detection[2][3]
-                    })
-                    
-                    
-                    x1 = str(c['p1'][0])
-                    y1 = str(c['p1'][1])
-                    x2 = str(c['p2'][0])
-                    y2 = str(c['p2'][1])
-                    x3 = str(c['p3'][0])
-                    y3 = str(c['p3'][1])
-                    x4 = str(c['p4'][0])
-                    y4 = str(c['p4'][1])
-                    
-                    coordStr = f"{x1}-{y1}-{x2}-{y2}-{x3}-{y3}-{x4}-{y4}"
-                    print (f"Found with confidence: {confidence}; Coords: {coordStr}")
-                    req('set_service_selfies_coordinates_generator',{
-                        'selfie_id': selfie['id'],
-                        'coordinates': coordStr
-                    })
-            except Exception as ex:
-                print (ex)
+            # Download selfie
+            download_url = "https://cdn.appealbot.net/"+selfie['filename']
+            tmpdirStr = str(tmpdir)
+            save_location = tmpdirStr+SLASH+selfie['filename']
+            urllib.request.urlretrieve(
+                download_url,
+                save_location
+            )
+            print ('[DETECTOR] Performing for '+selfie['filename'])
+            detections = darknet.performDetect(save_location)
+            #detections = [('papersheet', 0.9260228872299194, (586.1337890625, 1471.395751953125, 992.4650268554688, 593.9584350585938))]
+            if len(detections) < 1:
+                print ("Not found")
+                req('set_service_selfies_coordinates_generator',{
+                    'selfie_id': selfie['id'],
+                    'coordinates': None
+                });
+            else:
+                # Get detection with highest confidence ( bruh )
                 
+                detection = sorted(detections,key= lambda x: x[1], reverse = True)[0]
+                
+                confidence = str(int(detection[1]*100)) + "%"
+                
+                c = getAccurateBox(save_location,{
+                    "center_x":detection[2][0],
+                    "center_y":detection[2][1],
+                    "width":detection[2][2],
+                    "height":detection[2][3]
+                })
+                
+                
+                x1 = c['p1'][0]
+                y1 = c['p1'][1]
+                x2 = c['p2'][0]
+                y2 = c['p2'][1]
+                x3 = c['p3'][0]
+                y3 = c['p3'][1]
+                x4 = c['p4'][0]
+                y4 = c['p4'][1]
+                
+                coordStr = f"{x1}-{y1}-{x2}-{y2}-{x3}-{y3}-{x4}-{y4}"
+                print (f"Found with confidence: {confidence}; Coords: {coordStr}")
+                req('set_service_selfies_coordinates_generator',{
+                    'selfie_id': selfie['id'],
+                    'coordinates': coordStr
+                })
+        
                     
         sleep(10)
                     
