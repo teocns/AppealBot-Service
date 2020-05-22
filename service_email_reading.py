f8c6e4a0e4207d6cbd339d7dedc971ac1b504667
teocns <teocns@gmail.com>
2020-05-22 16:20:36 +0200

    Update service_email_reading.py

--- service_email_reading.py
+++ service_email_reading.py
@@ -54,7 +54,7 @@ while True:
         pop3handler(data['pop_server'],data['email'],data['email_password'],handleFbEmailFound,loginErrorCallback=handleLoginErrorCallback)
     except Exception as ex:
         print(f'[{prttime()}] Exception occured - Skipping')
-        
+        print(ex)
 
 
     sleep(10)
