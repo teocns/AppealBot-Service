035920ba0437900a1a321c956c93e5fffd8acccd
teocns <teocns@gmail.com>
2020-05-27 10:54:50 +0200

    Update service_unban_checker.py

--- service_unban_checker.py
+++ service_unban_checker.py
@@ -18,6 +18,11 @@ from api import req
 
 proxy_cookie_pairs = [
 	{
+		'cookie':'rur=FRC; mid=Xs3vrwALAAFEXqm8CoSf1t6btTnT; ig_did=C0945479-28C7-45E4-A25F-4E6BCC255ECE; urlgen="{\"2604:180:3:376:5639:7bcc:ec03:6abd\": 3842}:1jdnv8:w1O7HNI0-HNpMSlQKhFqlPZR05A"; ds_user_id=31874002324; sessionid=31874002324%3AmzcUxKyrSdXbJu%3A17',
+		'proxy_ip_port':'168.235.95.186:46650',
+		'proxy_user_pass':'MngbJ3:6PL9EF'
+	},
+	{
 		'cookie':'ig_did=A56068B1-24A3-4F27-BD32-144E52F2CD1F; csrftoken=jWSmxNZQ0slNflXBYII50EDRs5ASRvBn; mid=Xp8JxAALAAFSbjpiQFEY4tlaHmjq; ds_user_id=32069056468; sessionid=32069056468%3A3F4Yb2S17Gpkuy%3A4; urlgen="{\"196.19.178.42\": 19969}:1jdnVs:dTmEOrxb5mVDbtv8IIZG33Fr96o"',
 		'proxy_ip_port':'196.19.178.42:8000',
 		'proxy_user_pass':'9WTX7a:eGuqLS'
@@ -42,31 +47,35 @@ total_proxies = len(proxy_cookie_pairs)
 cur_proxy_index = 0
 
 while True:
-	cur_proxy_index = cur_proxy_index +1;
+	cur_proxy_index +=1;
 	
 	if cur_proxy_index > total_proxies -1:
 		cur_proxy_index = 0
+	
 	cur_proxy = proxy_cookie_pairs[cur_proxy_index]
 	afs = req('get_for_unban_checker_service')
 	if (afs):
 		print(f"Checking {afs['username']} with proxy {cur_proxy['proxy_ip_port']}..")
 		username = afs['username']
-
 		proxies = {
 	  		'https' : f"https://{cur_proxy['proxy_user_pass']}@{cur_proxy['proxy_ip_port']}",
 			'http' : f"https://{cur_proxy['proxy_user_pass']}@{cur_proxy['proxy_ip_port']}",
    		} 
-		exit(f"https://www.instagram.com/{username}/?__a=1")
+		print(f"https://www.instagram.com/{username}/")
+		
 		response = requests.get(
-			url=f'https://www.instagram.com/{username}/?__a=1',
+			url=f'https://www.instagram.com/{username}/',
 			headers={
-				'cookie': proxy_cookie_pairs[cur_proxy_index]['cookie'],
+				'cookie': cur_proxy['cookie'],
 				'host': 'www.instagram.com',
-						'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0'
+				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+
 			}
 		)
-		print(response.text)
-		if response.status_code == 200 and response.text != "{}":
+		if 'not-logged-in' in response.text:
+			print(' Not logged in!! Bad cookies. Status code :'+str(response.status_code))
+		
+		if response.status_code == 200 and 'window._sharedData = {"' in response.text:
 			print(f'[{username}] Is active, confirming unban')
 			req(
 				'register_unban_confirmation',
@@ -74,7 +83,7 @@ while True:
 					'ig_account_id': afs['id']
 				}
 			)
-		sleep(5)
+		sleep(10)
 	else:
 		print('Found no accounts..')
 		sleep(30)
