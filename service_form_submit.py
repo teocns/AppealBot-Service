daf34141345d1fbca219a4ac51b6e8aa9d8b3aa8
teocns <teocns@gmail.com>
2020-06-14 16:09:01 +0200

    Update service_form_submit.py

--- service_form_submit.py
+++ service_form_submit.py
@@ -18,85 +18,87 @@ while True:
     appeal_info = req('get_form_submit_service')
     
     #exit(json.dumps(appeal_info))
-    
-    if not appeal_info:
-        print(f"[{prttime()}] No appeal info found, sleeping 10 seconds")
-        sleep(10)
-    
-    else:
-        proxy = proxyHandler.getProxy()
-        if proxy:
-            print(f"[{appeal_info['instagram_account']['username']}] Submitting {appeal_info['appeal_form']['python_name']} with proxy {proxy.ip}:{proxy.port}...")
-        else:
-            print(f"[{appeal_info['instagram_account']['username']}] Submitting {appeal_info['appeal_form']['python_name']} without proxy...")
+    try:    
+        if not appeal_info:
+            print(f"[{prttime()}] No appeal info found, sleeping 10 seconds")
+            sleep(10)
         
-  
-        result = None
-        if appeal_info['appeal_form']['python_name'] == "form_two":
-            result = submit_form_two(
-                appeal_info['instagram_account']['username'], 
-                appeal_info['appeal_resource']['full_name'],
-                appeal_info['appeal_resource']['email'],
-                appeal_info['appeal_resource']['phone_number'],
-                appeal_info['spintax'],
-                proxy
-            )
         else:
-            result = submit_form_one(
-                appeal_info['instagram_account']['username'], 
-                appeal_info['appeal_resource']['full_name'],
-                appeal_info['appeal_resource']['email'],
-                proxy
-            )
 
-        if result == True:
-            # Form has been submitted, give feedback to the server
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'form_submitted',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(
-                f"[{prttime()}] {appeal_info['instagram_account']['username']} / Form submitted SUCCESSFULLY")
-        elif result == 'is_active':
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'is_active',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} is ALREADY ACTIVE")
-        elif result == 'is_inexistent':
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'is_inexistent',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} LOGIN FOR REVIEW")
-        elif result == "login_for_review":
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'login_for_review',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} USER LOGIN REQUIRED FOR REVIEW")
-        elif result == "confirm_its_you":
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'confirm_its_you',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} is INEXISTENT")
-        else:
-            req('callback_form_submit_service', data={
-                'appeal_process_id': appeal_info['id'],
-                'status': 'unknown',
-                'form_id': appeal_info['appeal_form']['id']
-            })
-            print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} Failed submitting form")
-        if not proxy:
-            print ('Sleeping 180 seconds because no proxy is available. Preventing eventual IP blocks.')
-            sleep(60)
+            proxy = proxyHandler.getProxy()
+            if proxy:
+                print(f"[{appeal_info['instagram_account']['username']}] Submitting {appeal_info['appeal_form']['python_name']} with proxy {proxy.ip}:{proxy.port}...")
+            else:
+                print(f"[{appeal_info['instagram_account']['username']}] Submitting {appeal_info['appeal_form']['python_name']} without proxy...")
+            
     
+            result = None
+            if appeal_info['appeal_form']['python_name'] == "form_two":
+                result = submit_form_two(
+                    appeal_info['instagram_account']['username'], 
+                    appeal_info['appeal_resource']['full_name'],
+                    appeal_info['appeal_resource']['email'],
+                    appeal_info['appeal_resource']['phone_number'],
+                    appeal_info['spintax'],
+                    proxy
+                )
+            else:
+                result = submit_form_one(
+                    appeal_info['instagram_account']['username'], 
+                    appeal_info['appeal_resource']['full_name'],
+                    appeal_info['appeal_resource']['email'],
+                    proxy
+                )
+
+            if result == True:
+                # Form has been submitted, give feedback to the server
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'form_submitted',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(
+                    f"[{prttime()}] {appeal_info['instagram_account']['username']} / Form submitted SUCCESSFULLY")
+            elif result == 'is_active':
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'is_active',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} is ALREADY ACTIVE")
+            elif result == 'is_inexistent':
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'is_inexistent',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} LOGIN FOR REVIEW")
+            elif result == "login_for_review":
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'login_for_review',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} USER LOGIN REQUIRED FOR REVIEW")
+            elif result == "confirm_its_you":
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'confirm_its_you',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} is INEXISTENT")
+            else:
+                req('callback_form_submit_service', data={
+                    'appeal_process_id': appeal_info['id'],
+                    'status': 'unknown',
+                    'form_id': appeal_info['appeal_form']['id']
+                })
+                print(f"[{prttime()}] / {appeal_info['instagram_account']['username']} Failed submitting form")
+            if not proxy:
+                print ('Sleeping 180 seconds because no proxy is available. Preventing eventual IP blocks.')
+                sleep(60)
+    except Exception ex:
+        print('Exception occured:\n'+str(ex))
 
 
 
